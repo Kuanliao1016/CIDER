649f347508fac0634eeb7786c75563fefbf35941
Kuan Liao <61463206+Kuanliao1016@users.noreply.github.com>
2025-06-25 00:30:50 +0800

    update

--- app.R
+++ app.R
@@ -24,20 +24,32 @@ ui <- fluidPage(
         choices = c("Static" = "static", "Change" = "change")
       ),
       
+      radioButtons(
+        inputId = "Comparison",
+        label = "Is your anchor comparing each PROM response to the baseline or to the previous time?",
+        choices = c("Baseline" = "baseline", "Previous time" = "last")
+      ),
+      
       actionButton("run", "Run Analysis")
     ),
     
     mainPanel(
-      plotOutput("scatter_plot"),
-      plotOutput("forest_plot"),
-      uiOutput("results_ui")
+      tabsetPanel(
+        tabPanel("Data Preview", tableOutput("headdf")),
+        tabPanel("Scatter Plot", plotOutput("scatter_plot")),
+        tabPanel("Forest Plot + CI Table",
+                 fluidRow(
+                   column(6, plotOutput("forest_plot")),
+                   column(6, tableOutput("ci_table"))
+                 )
+        )
+      )
     )
   )
 )
-
 ########## Server ##########
 server <- function(input, output) {
-  
+
   raw_data <- reactive({
     req(input$upload)
     read.csv(input$upload$datapath)
@@ -45,41 +57,118 @@ server <- function(input, output) {
   
   processed_data <- eventReactive(input$run, {
     df <- raw_data()
-    df %>%
+    df <- df %>%
       mutate(
         SSID = as.factor(SSID),
         time = as.integer(time),
-        PGI_C = as.integer(PGI_C),
-        PROM = as.integer(PROM)
-      ) %>%
-      group_by(SSID) %>%
-      mutate(
-        baseline = PROM[time == 0],
-        PROM_chg_from_bl = PROM - baseline
-      ) %>%
-      ungroup()
+        PROM = as.numeric(PROM)
+      )
+    
+    if (input$Anchor_type == "static") {
+      # Only process PGI_S columns
+      if (!"PGI_S" %in% names(df)) stop("Column 'PGI_S' not found in your data.")
+      df <- df %>%
+        mutate(PGI_S = as.numeric(PGI_S)) %>%
+        arrange(SSID, time) %>%
+        group_by(SSID) %>%
+        mutate(
+          baseline_PROM = first(PROM),
+          baseline_PGI_S = first(PGI_S),
+          PROM_chg = PROM - lag(PROM),
+          PROM_chg_from_bl = PROM - baseline_PROM,
+          PGI_S_chg = PGI_S - lag(PGI_S),
+          PGI_S_chg_from_bl = PGI_S - baseline_PGI_S
+        ) %>%
+        ungroup()
+    } else if (input$Anchor_type == "change") {
+      # Only process PGI_C columns
+      if (!"PGI_C" %in% names(df)) stop("Column 'PGI_C' not found in your data.")
+      df <- df %>%
+        mutate(PGI_C = as.numeric(PGI_C)) %>%
+        arrange(SSID, time) %>%
+        group_by(SSID) %>%
+        mutate(
+          baseline_PROM = first(PROM),
+          PROM_chg = PROM - lag(PROM),
+          PROM_chg_from_bl = PROM - baseline_PROM,
+          PGI_C_chg = PGI_C - lag(PGI_C)
+        ) %>%
+        ungroup()
+    } else {
+      stop("Unknown anchor type.")
+    }
+    df
   })
+  
+  
   
   model_fit <- reactive({
-    req(processed_data(), input$PROM_type)
+    req(processed_data(), input$PROM_type, input$Anchor_type, input$Comparison)
     df <- processed_data()
     
-    if (input$PROM_type == "continuous") {
+
+    if (input$PROM_type == "continuous" & input$Anchor_type == "change"& input$Comparison == "baseline") {
       lmer(PROM_chg_from_bl ~ PGI_C + (1 | SSID), data = df)
-    } else {
+    } else if (input$PROM_type == "continuous" & input$Anchor_type == "change" & input$Comparison == "last") {
+      lmer(PROM_chg ~ PGI_C + (1 | SSID), data = df)
+    } else if (input$PROM_type == "categorical" & input$Anchor_type == "change"& input$Comparison == "baseline") {
       lmer(PROM_chg_from_bl ~ as.factor(PGI_C) + (1 | SSID), data = df)
+    } else if (input$PROM_type == "categorical" & input$Anchor_type == "change"& input$Comparison == "last") {
+      lmer(PROM_chg ~ as.factor(PGI_C) + (1 | SSID), data = df)
+    } else if (input$PROM_type == "continuous" & input$Anchor_type == "static" & input$Comparison == "baseline") {
+      lmer(PROM_chg_from_bl ~ PGI_S_chg_from_bl + (1 | SSID), data = df)
+    } else if (input$PROM_type == "continuous" & input$Anchor_type == "static" & input$Comparison == "last") {
+      lmer(PROM_chg ~ PGI_S_chg + (1 | SSID), data = df)
+    } else if (input$PROM_type == "categorical" & input$Anchor_type == "static" & input$Comparison == "baseline") {
+      lmer(PROM_chg_from_bl ~ as.factor(PGI_S_chg_from_bl) + (1 | SSID), data = df)
+    } else if (input$PROM_type == "categorical" & input$Anchor_type == "static" & input$Comparison == "last") {
+      lmer(PROM_chg ~ as.factor(PGI_S_chg) + (1 | SSID), data = df)
+    } else {
+      NULL
     }
   })
-  
-  output$scatter_plot <- renderPlot({
+  output$headdf <- renderTable({
     req(processed_data())
-    df <- processed_data()
+    head(processed_data())
+  })
+  output$scatter_plot <- renderPlot({
+    req(processed_data(), input$PROM_type, input$Anchor_type, input$Comparison)
     
-    ggplot(df, aes(x = PGI_C, y = PROM_chg_from_bl)) +
-      geom_point(color = "grey30") +
-      geom_smooth(method = "lm", se = TRUE, color = "red") +
-      labs(x = "Patient Global Impression of Change (PGI-C)",
-           y = "Change in PROM from Baseline")
+    if (input$Anchor_type == "change"& input$Comparison == "baseline"){
+      df <- processed_data()
+      
+      ggplot(df, aes(x = PGI_C, y = PROM_chg_from_bl)) +
+        geom_point(color = "grey30") +
+        geom_smooth(method = "lm", se = TRUE, color = "red") +
+        labs(x = "Patient Global Impression of Change (PGI-C)",
+             y = "Change in PROM from Baseline")
+    } else if (input$Anchor_type == "change"& input$Comparison == "last"){
+      df <- processed_data()
+      
+      ggplot(df, aes(x = PGI_C, y = PROM_chg)) +
+        geom_point(color = "grey30") +
+        geom_smooth(method = "lm", se = TRUE, color = "red") +
+        labs(x = "Patient Global Impression of Change (PGI-C)",
+             y = "Change in PROM from last response")      
+    }
+      else if (input$Anchor_type == "static" & input$Comparison == "baseline"){
+      df <- processed_data()
+      
+      ggplot(df, aes(x = PGI_S_chg_from_bl, y = PROM_chg_from_bl)) +
+        geom_point(color = "grey30") +
+        geom_smooth(method = "lm", se = TRUE, color = "red") +
+        labs(x = "Patient Global Impression of Severity (PGI-S)",
+             y = "Change in PROM from Baseline")
+      }else{
+        df <- processed_data()
+        
+        ggplot(df, aes(x = PGI_S_chg, y = PROM_chg)) +
+          geom_point(color = "grey30") +
+          geom_smooth(method = "lm", se = TRUE, color = "red") +
+          labs(x = "Patient Global Impression of Severity (PGI-S)",
+               y = "Change in PROM from last response")  
+    }
+
   })
   
   output$results_ui <- renderUI({
@@ -94,16 +183,26 @@ server <- function(input, output) {
   
   output$forest_plot <- renderPlot({
     req(input$run)
-    req(model_fit())
+    req(model_fit(), input$PROM_type, input$Anchor_type)
     
     model <- model_fit()
     
-    if (input$PROM_type == "continuous") {
-      new_data <- data.frame(PGI_C = 1:7, SSID = NA)
-    } else {
-      new_data <- data.frame(PGI_C = as.factor(1:7), SSID = NA)
+    # Build new_data based on input
+    new_data <- if (input$PROM_type == "continuous" & input$Anchor_type == "change") {
+      data.frame(PGI_C = 1:7, SSID = NA)
+    } else if (input$PROM_type == "categorical" & input$Anchor_type == "change") {
+      data.frame(PGI_C = as.factor(1:7), SSID = NA)
+    } else if (input$PROM_type == "continuous" & input$Anchor_type == "static" & input$Comparison == "last") {
+      data.frame(PGI_S_chg = 1:7, SSID = NA)
+    } else if (input$PROM_type == "continuous" & input$Anchor_type == "static" & input$Comparison == "baseline") {
+      data.frame(PGI_S_chg_from_bl = 1:7, SSID = NA)
+    } else if (input$PROM_type == "categorical" & input$Anchor_type == "static" & input$Comparison == "last") {
+      data.frame(PGI_S_chg = as.factor(1:7), SSID = NA)
+    } else if (input$PROM_type == "categorical" & input$Anchor_type == "static" & input$Comparison == "baseline") {
+      data.frame(PGI_S_chg_from_bl = as.factor(1:7), SSID = NA)
     }
     
+    # Define bootstrapping function
     boot_fun <- function(fit) {
       predict(fit, newdata = new_data, re.form = NA)
     }
@@ -116,13 +215,23 @@ server <- function(input, output) {
     new_data$upper_ci <- cis[2, ]
     new_data$predicted <- predict(model, newdata = new_data, re.form = NA)
     
-    ggplot(new_data, aes(x = predicted, y = as.factor(PGI_C))) +
+    plot_data <- if (input$Anchor_type == "change") {
+      new_data %>% mutate(PGI = PGI_C)
+    } else if (input$Anchor_type == "static" & input$Comparison == "baseline") {
+      new_data %>% mutate(PGI = PGI_S_chg_from_bl)
+    } else if (input$Anchor_type == "static" & input$Comparison == "last") {
+      new_data %>% mutate(PGI = PGI_S_chg)
+    }
+    
+    ggplot(plot_data, aes(x = predicted, y = as.factor(PGI))) +
       geom_point(color = "grey10", size = 2) +
       geom_errorbarh(aes(xmin = lower_ci, xmax = upper_ci), height = 0.2, color = "grey30") +
       labs(
         x = "Predicted PROM Change",
-        y = "PGI-C",
-        title = paste("Forest Plot of Expected PROM Change by PGI-C (", input$PROM_type, ")")
+        y = ifelse(input$Anchor_type == "change", "PGI-C", "PGI-S"),
+        title = paste("Forest Plot of Expected PROM Change by",
+                      ifelse(input$Anchor_type == "change", "PGI-C", "PGI-S"),
+                      "(", input$PROM_type, ")")
       ) +
       theme_bw() +
       theme(
@@ -131,16 +240,23 @@ server <- function(input, output) {
       )
   })
   
-  
   output$ci_table <- renderTable({
     req(model_fit())
-    
     model <- model_fit()
     
-    new_data <- if (input$PROM_type == "continuous") {
+    # Define new_data as before
+    new_data <- if (input$PROM_type == "continuous" & input$Anchor_type == "change") {
       data.frame(PGI_C = 1:7, SSID = NA)
-    } else {
+    } else if (input$PROM_type == "categorical" & input$Anchor_type == "change") {
       data.frame(PGI_C = as.factor(1:7), SSID = NA)
+    } else if (input$PROM_type == "continuous" & input$Anchor_type == "static" & input$Comparison == "last") {
+      data.frame(PGI_S_chg = 1:7, SSID = NA)
+    } else if (input$PROM_type == "continuous" & input$Anchor_type == "static" & input$Comparison == "baseline") {
+      data.frame(PGI_S_chg_from_bl = 1:7, SSID = NA)
+    } else if (input$PROM_type == "categorical" & input$Anchor_type == "static" & input$Comparison == "last") {
+      data.frame(PGI_S_chg = as.factor(1:7), SSID = NA)
+    } else if (input$PROM_type == "categorical" & input$Anchor_type == "static" & input$Comparison == "baseline") {
+      data.frame(PGI_S_chg_from_bl = as.factor(1:7), SSID = NA)
     }
     
     boot_fun <- function(fit) {
@@ -155,15 +271,24 @@ server <- function(input, output) {
     new_data$upper_ci <- cis[2, ]
     new_data$predicted <- predict(model, newdata = new_data, re.form = NA)
     
-    new_data <- new_data%>%mutate(
-      PGI_C = PGI_C,
-      Expected_PROM = predicted,
-      lower_ci = lower_ci,
-      upper_ci = upper_ci
-    )%>%select(
-      PGI_C,Expected_PROM,lower_ci,upper_ci
-    )
-    new_data
+    if (input$Anchor_type == "change") {
+      new_data %>%
+        mutate(Expected_PROM = predicted) %>%
+        arrange(desc(as.numeric(PGI_C))) %>%
+        select(PGI_C, Expected_PROM, lower_ci, upper_ci)
+    } else if (input$Anchor_type == "static" & input$Comparison == "baseline") {
+      new_data %>%
+        mutate(PGI_S = PGI_S_chg_from_bl,
+               Expected_PROM = predicted) %>%
+        arrange(desc(as.numeric(PGI_S))) %>%
+        select(PGI_S, Expected_PROM, lower_ci, upper_ci)
+    } else if (input$Anchor_type == "static" & input$Comparison == "last") {
+      new_data %>%
+        mutate(PGI_S = PGI_S_chg,
+               Expected_PROM = predicted) %>%
+        arrange(desc(as.numeric(PGI_S))) %>%
+        select(PGI_S, Expected_PROM, lower_ci, upper_ci)
+    }
   }, striped = TRUE)
 }
 
